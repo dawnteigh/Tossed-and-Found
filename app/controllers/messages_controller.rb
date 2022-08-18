d538077e6db7dac6b8e8b214b9b43f676be71473
Donte Ladatto <donte279@gmail.com>
2022-08-18 16:50:28 -0500

    Message validations, routes, actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+
+  def index
+    if current_user
+      msgs = current_user.messages + Message.where(to: current_user.username).order("created_at")
+      render json: msgs, status: :ok
+    else
+      unauthorized
+    end
+  end
+
+  def create
+    if current_user
+      msg = current_user.messages.create!(msg_params)
+      render json: msg, status: :created
+    else
+      unauthorized
+    end
+  end
+
+  private
+
+  def msg_params
+    params.permit(:subject, :body, :to)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
+  validates :body, presence: true, length: { in: 6..300 }
+  validates :subject, presence: true, length: { maximum: 50 }
+  validates :to, presence: true
 end
