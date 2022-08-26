ed37376d25c870e26faef1df1169c0cc40be3af1
Donte Ladatto <donte279@gmail.com>
2022-08-25 19:55:12 -0500

    ScoreCard is aware of selectedCourse

--- client/src/ScoreCard.js
+++ client/src/ScoreCard.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from './context/user'
 
 const ScoreCard = () => {
 
+  const { selectedCourse, history } = useContext(UserContext)
+  const { id, name, location, holes } = selectedCourse
+  const [holesArray, setHolesArray] = useState([])
   
-  
+  useEffect(() => {
+    if (!selectedCourse) {
+      history.push('/courses')
+    } else {
+      setHolesArray(Array.from({length: holes}, (_, i) => i + 1))
+    }
+  }, [])
+
+  // TODO: create a form for each value in holesArray
+
+
   return (
-    <div>ScoreCard</div>
+    <div>
+      <h3>Currently playing at: {name}</h3>
+    </div>
   )
 }
 
