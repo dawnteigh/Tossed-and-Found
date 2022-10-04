f8b36aef377b44fe77632109ae2bf68cdca129c2
Donte Ladatto <donte279@gmail.com>
2022-10-03 21:32:16 -0500

    update user courses on new score

--- app/serializers/score_serializer.rb
+++ app/serializers/score_serializer.rb
@@ -1,4 +1,15 @@
 class ScoreSerializer < ActiveModel::Serializer
   attributes :id, :strokes, :par, :player, :created_at, :course
   belongs_to :user
+
+  def course
+    c = object.course
+    { 
+        id: c.id,
+        holes: c.holes,
+        name: c.name,
+        location: c.location,
+        best: c.personal_best(object.user.id)
+      }
+    end
 end
