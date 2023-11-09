fc501d8d3c6c3dd7b9aa3ae1e72b8fe21847cc98
Donte Ladatto <donte279@gmail.com>
2023-11-09 14:48:55 -0600

    remove user object from res, change img logic

--- app/serializers/disc_serializer.rb
+++ app/serializers/disc_serializer.rb
@@ -1,9 +1,16 @@
 class DiscSerializer < ActiveModel::Serializer
   attributes :id, :color, :make, :model, :weight, :disc_type, :img, :user_id, :finder_key, :lost
-  belongs_to :user
 
   def weight
     "#{object.weight}g"
+  end
+
+  def img
+    if object.image.attached?
+      return object.img_url
+    else
+      return object.img
+    end
   end
 
 end
