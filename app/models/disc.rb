7c5d62caeaecfd54e00fd2d28caaf37f99eabb7e
Donte Ladatto <donte279@gmail.com>
2023-12-17 10:12:57 -0600

    generate unique key before creation w/ securerandom

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -6,8 +6,24 @@ class Disc < ApplicationRecord
   validates :weight, numericality: true, length: { maximum: 3 }
   validates :image, content_type: { in: ['image/png', 'image/jpeg', 'image/jpg'], message: "file type must be .png, .jpeg, or .jpg"}, size: { less_than: 5.megabytes }
 
+  before_create :set_finder_key
+
+  private
+
   def img_url
     Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true) if image.attached?
+  end
+
+  def set_finder_key
+    self.finder_key = generate_key
+  end
+
+  def generate_key
+    loop do
+      token = SecureRandom.hex(4)
+
+      break token unless Disc.where(finder_key: token).exists?
+    end
   end
 
 end
