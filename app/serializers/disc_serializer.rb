8923ef7bc85ef1dc4968fc99300e57696abd2044
Donte Ladatto <donte279@gmail.com>
2023-12-17 10:13:33 -0600

    serialize user again to fix previous mistake

--- app/serializers/disc_serializer.rb
+++ app/serializers/disc_serializer.rb
@@ -1,6 +1,6 @@
 class DiscSerializer < ActiveModel::Serializer
   attributes :id, :color, :make, :model, :weight, :disc_type, :img, :user_id, :finder_key, :lost
-
+  belongs_to :user
   def weight
     "#{object.weight}g"
   end
